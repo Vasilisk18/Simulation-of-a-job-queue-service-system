599d0199fb706ded388399b40cf4543df3320649
Vasilisk18 <90246735+Vasilisk18@users.noreply.github.com>
2023-04-16 18:15:46 +0300

    Update test_hashtable.cpp

--- mp2-lab5-tables-master/Test/test_hashtable.cpp
+++ mp2-lab5-tables-master/Test/test_hashtable.cpp
@@ -11,3 +11,31 @@ TEST(KEYHASH, check_function)
 	key = k("abcde");
 	ASSERT_EQ(key, 95);
 }
+
+// Тест добавления элемента в хэш-таблицу
+TEST(HashTableTest, AddElementTest)
+{
+  HashTable<std::string, int, KeyHash> hashTable;
+  hashTable.add("test_key", 100);
+  int value = 0;
+  EXPECT_TRUE(hashTable.get("test_key", value));
+  EXPECT_EQ(value, 100);
+}
+
+// Тест поиска несуществующего ключа в хэш-таблице
+TEST(HashTableTest, GetNonExistingKeyTest)
+{
+  HashTable<std::string, int, KeyHash> hashTable;
+  int value = 0;
+  EXPECT_FALSE(hashTable.get("non_existing_key", value));
+}
+
+// Тест удаления элемента из хэш-таблицы
+TEST(HashTableTest, RemoveElementTest)
+{
+  HashTable<std::string, int, KeyHash> hashTable;
+  hashTable.add("test_key", 100);
+  EXPECT_TRUE(hashTable.existsKey("test_key"));
+  hashTable.remove("test_key");
+  EXPECT_FALSE(hashTable.existsKey("test_key"));
+}
